62270dd4f17806c20620f60f91bfe1eeaa73977d
alfredo <alfredo.ucar@gmail.com>
2015-10-06 21:14:47 +0200

    jugador

--- main.lua
+++ main.lua
@@ -2,38 +2,25 @@ require "libs/graphics"
 require "libs/physics"
 require "libs/sounds"
 
+require "world"
 require "player"
-require "shadow"
 
-function love.focus(f) gameIsPaused = not f end
+objects = {}
 
 function love.load()
-	LOAD_SCREEN()
-	player.load()
-	-- song:play()
+	graphics_set_up()
 end
 
 function love.keypressed(key)
-	player.shadow(key)
+	--player.shadow(key)
 end
 
 function love.update(dt)
-if gameIsPaused then return end
-	if player.health > 0 then
-	 UPDATE_PLAYER(dt)
-	--  UPDATE_BULLET(dt)
-	else
-	love.graphics.setColor(0, 255, 0, 255)
-		love.graphics.print("GAME OVER!!", 10, 250, 0, 2, 2)
-	end
+	world:update(dt) --this puts the world into motion
 end
 
 
 function love.draw()
- 	DRAW_PLAYER()
-	DRAW_SHADOW()
+ 	player.draw()
 end
 
-function love.quit()
-	print("Thanks for playing ^^")
-return end
--- player.lua
+++ player.lua
@@ -1,111 +1,16 @@
+local size = 50 -- square
+-- center coordinates
+local x = screenWidth/2
+local y = screenHeight-size/2
+local color = black 
+
 player = {}
-
-function player.load()
-	player.width = 50
-	player.height = 50
-	player.x = screenWidth / 2
-	player.y = ground - 50
-	player.xvel = 0
-	player.yvel = 0
-	player.friction = 9.5
-	player.speed = 2250
-	player.color = black
-	player.health = 150
-	player.jump = 500
-
-end
-
-function player:paint(color)
-	player.color = color
-end
+player.body = love.physics.newBody(world, x, y, "dynamic") 
+player.shape = love.physics.newPolygonShape( {x-size/2, y-size/2, x+size/2, y-size/2, x+size/2, y+size/2, x-size/2, y+size/2} )
+player.fixture = love.physics.newFixture(player.body, player.shape);
 
 function player.draw()
-	if player.health > 0 then
-		rectangle(player)
-	end
+	love.graphics.setColor(color)
+	love.graphics.polygon("fill", player.shape:getPoints())
 end
 
-function player.physics(dt)
-	gravity(dt, player)
-end
-
-function player.boundary()
-
-	--colision for shadows
-	for i,v in ipairs(shadow) do
-		if(colision(player,v))then
-			if player.y == v.y then
-				if player.x < v.x then --left
-					player.x = (v.x - v.width)
-				end
-				if player.x > v.x then --right
-					player.x = (v.x + v.width)
-				end
-			elseif player.y > (v.y - v.height) then
-				player.y = (v.y - v.height)
-			end
-		end
-	end
-
-	--Screen limits for x and y
-	if player.x < 0 then
-		player.x = 0
-		player.xvel = 0
-	end
-	if player.y < 0 then
-		player.y = 0
-		-- player.yvel = 0
-	end
-	if player.x + player.width > screenWidth then
-		player.x = screenWidth - player.width
-	end
-	if player.y + player.height > screenHeight then
-		player.y = screenHeight- player.height
-	end
-end
-
-function player.move(dt)
-	--eje x
-	if love.keyboard.isDown('d') and player.xvel < player.speed then
-		player.xvel = player.xvel + player.speed * dt
-	end
-	if love.keyboard.isDown('a') and player.xvel > -player.speed then
-		player.xvel = player.xvel - player.speed * dt
-	end
-
-	if love.keyboard.isDown(' ') and player.yvel == 0 then
-		player.yvel = player.jump
-	end
-end
-
-function player.life(dt)
-	if player.health < 100 then
-		player:paint(red)
-	end
-
-	if player.health == 0 then
-		love.quit()
-	end
-end
-
-function player.shadow(key)
-	if key == 's' then
-		--check if there is a shadow there or near
-		shadow:spawn(player.x,player.y,player.color)
-		player.health = player.health - 10
-	end
-end
-
-
---PARENT FUNCTIONS
-function DRAW_PLAYER()
-	player.draw()
-	player:paint(blue)
-end
-
-function UPDATE_PLAYER(dt)
-	player.physics(dt)
-	player.boundary()
-	player.move(dt)
-	player.life(dt)
-end
