6f2cae68bfbec7d7e8b7d2141d9591b067dcf0fa
alfredo <alfredo.ucar@gmail.com>
2015-10-13 10:31:52 +0200

    creación de sombras al soltar el botón 's' en lugar de al pulsar(evita creación continua). Botón de escape

--- controls.lua
+++ controls.lua
@@ -6,10 +6,19 @@ function keyboardControls()
 	if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
 		player.moveLeft()
 	end
-	if love.keyboard.isDown("s") then
-		player.createShadow()
-	end	
+	--if love.keyboard.isDown("s") then
+	--	player.createShadow()
+	--end	
 	if love.keyboard.isDown(" ")  or love.keyboard.isDown("w") or love.keyboard.isDown("up") then
 		player.jump()
+	end
+end
+
+function love.keyreleased(key)
+	if key == "escape" then
+		love.event.quit()
+	end
+	if key == "s" then
+		player.createShadow()
 	end
 end
