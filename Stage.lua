2d5b20ea31b009377f1e2ef2df18700c38e68675
alfredo <alfredo.ucar@gmail.com>
2015-10-20 10:59:33 +0200

    nueva propiedad color y metodo para cambiarlo

--- Stage.lua
+++ Stage.lua
@@ -1,9 +1,11 @@
---------------------------------------------------
---		STAGE				--
---------------------------------------------------
--- Determine physic world boundaries and 	--
--- contains game level objects			--
---------------------------------------------------
+------------------------------------------------------------------------------------------
+--		STAGE									--
+------------------------------------------------------------------------------------------
+-- Determine physic world boundaries and contains game level objects.			--
+-- It is assumed that there is global variable accessible from 				--
+-- here:										--
+-- 	-player -> declared in main							--
+------------------------------------------------------------------------------------------
 
 --------------------------------------------------
 -- 		STATIC VARIABLES		--
@@ -18,20 +20,24 @@ local RIGHT_WALL = "Right wall"
 --		CONSTRUCTOR			--
 --------------------------------------------------
 -- world: 	world to wich the stage belongs
+-- color:	background color
 -- width: 	(optional) stage width and world boundary
 -- height: 	(optional) stage height and world boundary
 -- returns => stage instance or nil if cannot create the stage
-function Stage(world, width, height)
+function Stage(world, color, width, height)
 	local self = {}
-	if not world then return nil end						-- check required param
+	if not world or not color then return nil end					-- check required params
 	
 	--check optional params and fill with default vaules if necessary
 	width = width or love.graphics.getWidth()
 	height = height or love.graphics.getHeight()
-
+	
+	love.graphics.setBackgroundColor(color)						-- set background color
+	player.setColor(invertColor(color))						-- set player with negative color
 	--------------------------------------------------
 	-- 		   PROPERTIES			--
 	--------------------------------------------------
+	local color = color
 	local ground = {}								-- ground element (bottom limit)
 	ground.body = love.physics.newBody(world) 					-- love2d body
 	ground.shape = love.physics.newEdgeShape( 0, height, width, height )		-- love2d shape
@@ -55,6 +61,25 @@ function Stage(world, width, height)
 	rightWall.shape = love.physics.newEdgeShape( width, 0, width, height )		-- love2d shape
 	rightWall.fixture = love.physics.newFixture(rightWall.body, rightWall.shape);	-- love2d fixture
 	rightWall.fixture:setUserData(RIGHT_WALL)					-- set ID
+
+	--------------------------------------------------
+	-- 		INSTANCE METHODS		--
+	--------------------------------------------------
+	
+	-- Changes the stage color. That implies a change color of the player too.
+	-- c:	(optional) new color. Random if not defined.
+	function self.changeColor(c)
+		if not c then					-- color not specified
+			newcolor = randomColor()		-- get a random color
+			while newcolor == color do		-- while same color...
+				newcolor = randomColor()	--	...get new random color
+			end
+			c=newcolor				-- set c with random color
+		end
+		color=c						-- assign new color
+		love.graphics.setBackgroundColor(color)		-- set background color
+		player.setColor(invertColor(color))		-- set player color with stage negative color
+	end
 
 	return self									-- stage instance
 end
