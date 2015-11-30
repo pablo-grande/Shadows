ecd3212842561903874d8a5447c0730bd18846d2
alfredo <alfredo.ucar@gmail.com>
2015-11-30 14:45:36 +0100

    en lugar de indicar la posicion de la sombra se pasa la funcion de spawn usada calcular la posicion

--- Shadow.lua
+++ Shadow.lua
@@ -1,54 +1,58 @@
 require "Square"			-- import base class
+require "libs/spawn"		-- import spawn functions
 
 --------------------------------------------------
 -- 		STATIC VARIABLES		--
 --------------------------------------------------
 
 local CLASS_NAME = "Shadow"
-local COUNT = 0				-- instances generated, used for ID
-local SIZE = 50				-- default size
-local X = love.graphics.getWidth()/2	-- default X
-local Y = love.graphics.getHeight()/2	-- default Y
+local COUNT = 0					-- instances generated, used for ID
+local SIZE = 50					-- default size
 local COLOR = black 			-- default color
-
+local SPAWN = spawnBehindOrNear	-- default spawn function. See spawn models: 'libs/spawn'
 
 --------------------------------------------------
 --		CONSTRUCTOR			--
 --------------------------------------------------
 -- world: world to wich the player belongs
--- x,y	: (optional)initial player position in world
+-- player: player who spawned the shadow
 -- size	: (optional)shadow size (square form)
 -- color: (optional)color used to paint the shadow on screen
+-- spawn: (optional)spawn function used
 -- returns => Shadow instance OR nil if cannot create the shadow
-function Shadow(world, x, y, size, color)
-	
-	if not world then return nil end			-- check required param
-	
+function Shadow(world, player, size, color, spawn)
+
+	if not world or not player then return nil end			-- check required param
+
 	--check optional params and fill with default vaules if necessary
-	x = x or X
-	y = y or Y
 	size = size or SIZE
 	color = color or COLOR
-	
+	spawn = spawn or SPAWN
+
 	--------------------------------------------------
 	-- 		   INHERITANCE			--
 	--------------------------------------------------
+	local x,y = spawn(player)								-- get position
+	if (x<0 or y<0) then return nil end						-- cannot place shadow
 	local self = Square(x, y, size, color)					-- extends Square
-	if not self then return nil end						-- failure(self should be something at this point!)
-	
-	COUNT = COUNT + 1							-- increment number of instaces generated
-	
+	if not self then return nil end							-- failure(self should be something at this point!)
+
+	COUNT = COUNT + 1										-- increment number of instaces generated
+
 	--------------------------------------------------
 	-- 		   PROPERTIES			--
 	--------------------------------------------------
-	local body = love.physics.newBody(world, x,y, "kinematic")		-- love2d body
+	local body = love.physics.newBody(world, x,y, "kinematic")			-- love2d body
 	local shape = love.physics.newRectangleShape( 0,0, size, size )		-- love2d shape
-	local fixture = love.physics.newFixture(body, shape)			-- love2d fixture
-	fixture:setUserData(CLASS_NAME..COUNT)					-- set ID = CLASS_NAME + number
+	local fixture = love.physics.newFixture(body, shape)				-- love2d fixture
+	fixture:setUserData(CLASS_NAME..COUNT)								-- set ID = CLASS_NAME + number
 
 	--------------------------------------------------
 	-- 		INSTANCE METHODS		--
 	--------------------------------------------------
+
+	-- Set spawn model
+	self.spawn = spawnBehindOrNear	-- behindOrNear model. For more info see: 'libs/spawn'
 
 	-- Draw the shadow
 	function self.draw()
@@ -63,10 +67,10 @@ end
 -- 		STATIC METHODS			--
 --------------------------------------------------
 
--- Determine if the element is a shadow 
+-- Determine if the element is a shadow
 -- f:	element's fixture
 -- returns => true if shadow
 function isShadow(f)
-	data = f:getUserData()							-- get ID
+	data = f:getUserData()												-- get ID
 	return string.sub(data,1,string.len(CLASS_NAME))==CLASS_NAME		-- split ID and check if first part is equal to class name
 end
